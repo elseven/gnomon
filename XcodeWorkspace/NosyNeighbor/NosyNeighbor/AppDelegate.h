13d9f690956f67935242aef7aeadd18b27f09f01
elseven <elliott2@uga.edu>
2014-10-25 17:43:34 -0400

    Added Titanium workspace and stuff

---
+++ XcodeWorkspace/NosyNeighbor/NosyNeighbor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NosyNeighbor
+//
+//  Created by Elliott Tanner on 10/25/14.
+//  Copyright (c) 2014 Elliott Tanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
