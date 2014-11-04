3b03fc012d058770801d8cfc004e30d42fbc3cb3
elseven <elliott2@uga.edu>
2014-11-04 11:13:24 -0500

    temp

---
+++ UnityStuf/gnomon/Assets/Scripts/AddSchool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddSchool : MonoBehaviour
+{
+
+
+
+		public GameObject SimpleSchoolContainer;
+		public GameObject CompareSchoolPanel;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
