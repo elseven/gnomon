65c4fc847c2fc598c39eb90262a56cf173afc1e3
elseven <elliott2@uga.edu>
2014-11-06 07:51:00 -0500

    login almost done

--- UnityStuf/gnomon/Assets/Scripts/LoginHandler.cs
+++ UnityStuf/gnomon/Assets/Scripts/LoginHandler.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LoginHandler : MonoBehaviour {
+public class LoginHandler : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+
+		private string correctEmail = "bob@uga.edu";
+		private string correctPassword = "p@ssword1";
 	
-	}
+		public string email;
+		public string password;
+		
+		public UILabel emailUI;
+		public UILabel passwordUI;
 	
-	// Update is called once per frame
-	void Update () {
 	
-	}
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	
+	
+		public void Submit ()
+		{
+			
+				Debug.Log ("Email = " + email);
+				Debug.Log ("Password = " + password);
+				
+				/*
+		emailUI.color = Color.black;
+		passwordUI.color = Color.black;*/
+		
+				if (CheckLogin ()) {
+						//TODO: LOAD SCENE
+				} else {
+						//TODO: ADD ERROR MESSAGE
+						emailUI.color = Color.red;
+						passwordUI.color = Color.red;
+				}
+	
+		
+		}
+		
+		public void SetEmail (string tempEmail)
+		{
+				email = tempEmail;
+		}
+		
+		public void SetPassword (string tempPassword)
+		{
+				password = tempPassword;
+		}
+		
+		private bool CheckLogin ()
+		{
+				return email.Equals (correctEmail) && password.Equals (correctPassword);
+		}
 }
