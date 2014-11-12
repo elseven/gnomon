797dbb54480f702065eb620d5c959cbdaaf1f711
elseven <elliott2@uga.edu>
2014-11-12 17:17:00 -0500

    V2 with tabs started

---
+++ UnityStuf/gnomon_v2/Assets/Scripts/ViewControl/HomeControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeControl : MonoBehaviour
+{
+
+		//FIXME: MINI GRAPHS! AND MAKE THE BUTTONS OPEN A GRAPHS PAGE.
+		
+
+
+		public GameObject SchoolOverlay;
+		public GameObject BuildingOverlay;
+		public GameObject RoomOverlay;
+		
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		void Awake ()
+		{
+		
+		
+		}
+	
+	
+	
+		public void ViewSchoolGraph ()
+		{
+				StartCoroutine ("ActivateSchoolOverlay");
+				
+				//FIXME: NOT IMPL
+				//DISPLAY GRAPH PAGE
+		}
+	
+	
+		public void ViewBuildingGraph ()
+		{
+				//FIXME: NOT IMPL
+				//FIXME: NOT LINKED
+		}
+	
+	
+		public void ViewRoomGraph ()
+		{
+				//FIXME: NOT IMPL
+				//FIXME: NOT LINKED
+		}
+
+		
+	#region Coroutines
+		
+		IEnumerator ActivateSchoolOverlay ()
+		{
+				SchoolOverlay.SetActive (true);
+				yield return new WaitForSeconds (.3f);
+				SchoolOverlay.SetActive (true);
+		}
+
+
+
+#endregion
+}
+
---
+++ UnityStuf/gnomon_v2/Assets/Scripts/ViewControl/TabControl.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabControl : MonoBehaviour
+{
+
+	
+		public GameObject HomeTab;
+		public GameObject TeamsTab;
+		public GameObject MatchesTab;
+	
+	
+		public GameObject HomePanel;
+		public GameObject TeamsPanel;
+		public GameObject MatchesPanel;
+	
+	
+
+
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		void Awake ()
+		{
+				ActivateHome ();
+		
+		}
+		
+		
+		
+		public void ActivateHome ()
+		{
+				HomePanel.SetActive (true);
+				TeamsPanel.SetActive (false);
+				MatchesPanel.SetActive (false);
+			
+		}
+		
+		
+		
+		
+		public void ActivateTeams ()
+		{
+				HomePanel.SetActive (false);
+				TeamsPanel.SetActive (true);
+				MatchesPanel.SetActive (false);
+		
+		}
+		
+		
+		public void ActivateMatches ()
+		{
+				HomePanel.SetActive (false);
+				TeamsPanel.SetActive (false);
+				MatchesPanel.SetActive (true);
+		}
+		
+		
+		
+		
+		
+}
