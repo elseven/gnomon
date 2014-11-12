121d3d9690e1b359e143df5e4ce9924593ee1047
elseven <elliott2@uga.edu>
2014-11-12 17:56:53 -0500

    temp

---
+++ UnityStuf/gnomon_v2/Assets/Scripts/Graphing/MiniGraph.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+public class MiniGraph : MonoBehaviour
+{
+
+
+		float left;
+		float right;
+		float top;
+		float bottom;
+		float width;
+		float height;
+
+		UISprite canvas;
+		// Use this for initialization
+		void Start ()
+		{
+				canvas = gameObject.GetComponent<UISprite> ();
+		
+				width = canvas.width;
+				height = canvas.height;
+				
+				
+				left = canvas.transform.position.x;
+				right = left + width;
+				
+				top = canvas.transform.position.y;
+				bottom = top + height;
+				
+				
+				Vector2[] points = new Vector2[20];
+				
+				
+				
+				VectorLine.SetLine (Color.green, points);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
---
+++ UnityStuf/testing_stuff/Assets/Testing123.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+public class Testing123 : MonoBehaviour
+{
+
+		void Start ()
+		{
+				VectorLine.SetLine (Color.green, new Vector2 (0, 0), new Vector2 (Screen.width - 1, Screen.height - 1));
+		}
+}
